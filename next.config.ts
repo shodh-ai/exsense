79f880ca36e128a0f715d60abd61345483ef64ba
Dr Sudhanshu <drsudhanshu@Drs-MacBook-Air.local>
2025-09-01 19:21:39 +0530

    suggested actions

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,22 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/:path*', 
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            // A slightly more permissive but still very secure alternative
+            value: `connect-src 'self' https: wss: ws://34.93.14.200:6901;`,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
