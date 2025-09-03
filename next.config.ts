f53e232474fc82f14c0a92f373933665c5788bd8
Arastu Sharma <arastu@mygyanvihar.com>
2025-09-03 15:30:48 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,22 +5,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-
-  async headers() {
-    return [
-      {
-        source: '/:path*', 
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            // A slightly more permissive but still very secure alternative
-            value: `connect-src 'self' https: wss: ws://34.93.14.200:6901;`,
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
